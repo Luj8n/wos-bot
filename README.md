a29b9d916303849bee5332abedbecc8ced08f17d
Luj8n <l.milieska@gmail.com>
2021-08-28 22:59:38 +0300

    features: min and max count, bonus letter count

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 1. Make sure that you have installed rust with [rustup](https://rustup.rs/)
 2. Add a `.env` file and fill it with your own data
 3. Run `cargo run --release` to run the bot
+
+## How to use it
+
+- Type: `!guess {letters} {min_count} {max_count} {bonus_letter_count} {word_list_choice}`
--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,27 @@ fn get_env_var(name: &str) -> String {
   return dotenv::var(name).expect(&format!("Couldn't find {} variable in the .env file", name));
 }
 
-fn word_can_be_formed(letters: &HashMap<u8, usize>, word: &HashMap<u8, usize>) -> bool {
-  word
-    .iter()
-    .all(|e| letters.contains_key(e.0) && &letters[e.0] >= e.1)
+fn get_word_difference(letters: &HashMap<u8, usize>, word: &HashMap<u8, usize>) -> usize {
+  word.iter().fold(0, |acc, e| {
+    if !letters.contains_key(e.0) {
+      acc + e.1
+    } else {
+      if &letters[e.0] >= e.1 {
+        acc
+      } else {
+        acc + e.1 - letters[e.0]
+      }
+    }
+  })
 }
 
-fn get_possible_words(word: &str, word_list_choice: &str) -> Result<Vec<String>, std::io::Error> {
+fn get_possible_words(
+  word: &str,
+  min_count: usize,
+  max_count: usize,
+  bonus_letter_count: usize,
+  word_list_choice: &str,
+) -> Result<Vec<String>, std::io::Error> {
   let start_time = Instant::now();
   let text = fs::read_to_string(format!("./word-lists/{}.txt", word_list_choice))?;
 
@@ -27,13 +41,10 @@ fn get_possible_words(word: &str, word_list_choice: &str) -> Result<Vec<String>,
 
   let letters = word.to_lowercase().bytes().counts();
 
-  let min_count = 4;
-  let max_count = word.len();
-
   let selected = word_list
     .filter(|w| {
       (min_count..=max_count).contains(&w.len())
-        && word_can_be_formed(&letters, &w.bytes().counts())
+        && get_word_difference(&letters, &w.bytes().counts()) <= bonus_letter_count
     })
     .sorted()
     .sorted_by(|a, b| Ord::cmp(&a.len(), &b.len()))
@@ -95,39 +106,52 @@ pub async fn main() {
             match &command.to_lowercase()[1..] {
               "guess" => {
                 if let Some(word) = words.get(1) {
-                  if let Some(word_list_choice) = words.get(2) {
-                    match get_possible_words(word, word_list_choice) {
-                      Ok(possible_words) => {
-                        // for w in possible_words {
-                        //   client.say(msg.channel_login.clone(), w).await.unwrap();
-                        //   thread::sleep(time::Duration::from_millis(250));
-                        // }
-                        let mut char_count = 0;
-                        let mut segments: Vec<Vec<String>> = vec![vec![]];
+                  if let Some(Ok(min_count)) = words.get(2).map(|x| x.parse()) {
+                    if let Some(Ok(max_count)) = words.get(3).map(|x| x.parse()) {
+                      if let Some(Ok(bonus_letter_count)) = words.get(4).map(|x| x.parse()) {
+                        if let Some(word_list_choice) = words.get(5) {
+                          match get_possible_words(
+                            word,
+                            min_count,
+                            max_count,
+                            bonus_letter_count,
+                            word_list_choice,
+                          ) {
+                            Ok(possible_words) => {
+                              // for w in possible_words {
+                              //   client.say(msg.channel_login.clone(), w).await.unwrap();
+                              //   thread::sleep(time::Duration::from_millis(250));
+                              // }
 
-                        possible_words.iter().for_each(|x| {
-                          char_count += x.len() + 1;
+                              let mut char_count = 0;
+                              let mut segments: Vec<Vec<String>> = vec![vec![]];
 
-                          if char_count >= 500 {
-                            segments.push(vec![]);
-                            char_count -= 500;
+                              possible_words.iter().for_each(|x| {
+                                char_count += x.len() + 1;
+
+                                if char_count >= 500 {
+                                  segments.push(vec![]);
+                                  char_count -= 500;
+                                }
+
+                                segments
+                                  .last_mut()
+                                  .expect("Vec is for some reason empty?")
+                                  .push(x.to_owned());
+                              });
+
+                              for segment in segments {
+                                client
+                                  .say(msg.channel_login.clone(), segment.join(" "))
+                                  .await
+                                  .expect("Error sending message");
+                              }
+                            }
+                            Err(error) => {
+                              println!("{:?}", error);
+                            }
                           }
-
-                          segments
-                            .last_mut()
-                            .expect("Vec is for some reason empty?")
-                            .push(x.to_owned());
-                        });
-
-                        for segment in segments {
-                          client
-                            .say(msg.channel_login.clone(), segment.join(" "))
-                            .await
-                            .expect("Error sending message");
                         }
-                      }
-                      Err(error) => {
-                        println!("{:?}", error);
                       }
                     }
                   }
